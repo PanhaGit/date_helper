dcd67cc3605764b4f0f9c8ef9013a6d5a3f27a20
Panha <thopanha123@gmail.com>
2025-09-26 16:17:17 +0700

    fix version intl

--- lib/date_helper.dart
+++ lib/date_helper.dart
@@ -1,8 +1,7 @@
 /// Author: THO Panha
-/// student from Western University in Cambodia
+/// Student from Western University
 /// Developer: WU Mobile
 /// Package: Date Translate
-/// copy right @PanhaGit 2025
 library;
 
 import 'package:flutter/widgets.dart';
@@ -10,17 +9,14 @@ import 'package:intl/intl.dart';
 import 'l10n/app_localizations.dart';
 
 class DateHelper {
-  /// Formats [dateTime] using the current locale
   static String formatDateTime(BuildContext context, DateTime dateTime) {
     final locale = Localizations.localeOf(context).toString();
     final formatter = DateFormat('MMM dd, yyyy - hh:mm a', locale);
     return formatter.format(dateTime);
   }
 
-  /// Returns time ago string, e.g., "5 minutes ago" / "5 នាទីមុន"
   static String formatTimeAgo(BuildContext context, DateTime? date) {
     final l10n = AppLocalizations.of(context)!;
-
     if (date == null) return l10n.unknownTime;
 
     final now = DateTime.now();
@@ -36,7 +32,6 @@ class DateHelper {
     ).format(date);
   }
 
-  /// Formats full date with time using current locale
   static String formatFullDate(BuildContext context, DateTime? date) {
     final l10n = AppLocalizations.of(context)!;
     if (date == null) return l10n.unknownDate;
@@ -46,7 +41,6 @@ class DateHelper {
     ).add_jm().format(date);
   }
 
-  /// Detailed date formatting (no localization)
   static String formatDetailedDate(DateTime? date) {
     if (date == null) return 'Unknown date';
     return DateFormat('MMMM d, y • h:mm a').format(date);
